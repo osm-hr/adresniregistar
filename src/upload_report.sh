dcbf794cf3b05da516a2fcd2763597b5542190be
Branko Kokanovic <branko@kokanovic.org>
2023-03-18 23:53:56 +0100

    dodajem brisanje prekopiranog fajla na server

--- src/upload_report.sh
+++ src/upload_report.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 current_date=`date +%Y%m%d`
 echo $current_date
 tar -czf data/report$current_date.tar.gz -C data/report/ .
-scp data/report$current_date.tar.gz kokanovic:/home/branko/ar
-ssh kokanovic "tar -xzf ar/report$current_date.tar.gz -C /var/www/sites/openstreetmap.rs/download/ar/"
+scp data/report$current_date.tar.gz kokanovic:/home/branko/ar/report$current_date.tar.gz
+ssh kokanovic "tar -xzf /home/branko/ar/report$current_date.tar.gz -C /var/www/sites/openstreetmap.rs/download/ar/"
+ssh kokanovic "rm -f /home/branko/ar/report$current_date.tar.gz"
 
