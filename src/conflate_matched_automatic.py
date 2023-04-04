9841fa27f8a180fbd0fb56432c4603550e9b45ef
Branko Kokanovic <branko@kokanovic.org>
2023-04-04 23:41:40 +0200

    Neinteraktivno izvrsavanje po potrebi

--- src/conflate_matched_automatic.py
+++ src/conflate_matched_automatic.py
@@ -2,11 +2,14 @@
 
 import argparse
 import os
+import time
 
 import osmapi
 import pandas as pd
 
 from common import load_mappings, normalize_name_latin, cyr2lat
+
+INTERACTIVE = True  # Change if you know what are you doing
 
 
 def do_opstina(data_path, street_mappings, opstina):
@@ -18,7 +21,7 @@ def do_opstina(data_path, street_mappings, opstina):
     for naselje_name, df_naselje in df_opstina.groupby('rgz_naselje'):
         naselje_name_lat = cyr2lat(naselje_name)
         print(f"Processing municipality {opstina} and settlement {naselje_name_lat}")
-        api = osmapi.OsmApi(passwordfile='osm-password', changesetauto=True, changesetautosize=20, changesetautotags={
+        api = osmapi.OsmApi(passwordfile='osm-password', changesetauto=True, changesetautosize=100, changesetautotags={
             "comment": f"RS address import in {opstina}/{naselje_name_lat} (adding ref:RS:kucni_broj to existing addresses, https://lists.openstreetmap.org/pipermail/imports/2023-March/007187.html)",
             "tag": "mechanical=yes",
             "source": "RGZ_AR"
@@ -52,8 +55,12 @@ def do_opstina(data_path, street_mappings, opstina):
             print(f'https://www.openstreetmap.org/{entity_type}/{entity_id}')
             accepted = False
             while True:
-                response = input(
-                    f"({i}/{len(df_naselje)}) Are you sure you want to add ref:RS:kucni_broj={rgz_kucni_broj} to {entity['tag']['addr:street']} {entity['tag']['addr:housenumber']} (Y/n/c)?")
+                if INTERACTIVE:
+                    response = input(f"({i}/{len(df_naselje)}) Are you sure you want to add ref:RS:kucni_broj={rgz_kucni_broj} to {entity['tag']['addr:street']} {entity['tag']['addr:housenumber']} (Y/n/c)?")
+                else:
+                    print(f"({i}/{len(df_naselje)}) +ref:RS:kucni_broj={rgz_kucni_broj} @ {entity['tag']['addr:street']} {entity['tag']['addr:housenumber']}")
+                    response = 'y'
+                    time.sleep(1)
                 if response == '' or response.lower() == 'y' or response.lower() == u'з':
                     accepted = True
                 if response.lower() == u'c' or response.lower() == u'ц':
@@ -68,14 +75,27 @@ def do_opstina(data_path, street_mappings, opstina):
 
             entity['tag']['ref:RS:kucni_broj'] = rgz_kucni_broj
 
+            # Check street name and ask for it
             proper_street_name = street_mappings[address['rgz_ulica']]
             if proper_street_name != entity['tag']['addr:street']:
-                response = input(f"({i}/{len(df_naselje)}) Do you also want to change addr:street from {entity['tag']['addr:street']} to {proper_street_name} (Y/n)?")
+                if INTERACTIVE:
+                    response = input(f"({i}/{len(df_naselje)}) Do you also want to change addr:street from {entity['tag']['addr:street']} to {proper_street_name} (Y/n)?")
+                else:
+                    print(f"({i}/{len(df_naselje)}) Changing addr:street: {entity['tag']['addr:street']} => {proper_street_name}")
+                    response = 'y'
+                    time.sleep(1)
                 if response == '' or response.lower() == 'y' or response.lower() == u'з':
                     entity['tag']['addr:street'] = proper_street_name
+
+            # Check housenumber and ask for it
             proper_housenumber = normalize_name_latin(address['rgz_kucni_broj'])
             if proper_housenumber != entity['tag']['addr:housenumber']:
-                response = input(f"({i}/{len(df_naselje)}) Do you also want to change addr:housenumber from {entity['tag']['addr:housenumber']} to {proper_housenumber} (Y/n)?")
+                if INTERACTIVE:
+                    response = input(f"({i}/{len(df_naselje)}) Do you also want to change addr:housenumber from {entity['tag']['addr:housenumber']} to {proper_housenumber} (Y/n)?")
+                else:
+                    print(f"({i}/{len(df_naselje)}) Changing addr:housenumber: {entity['tag']['addr:housenumber']} => {proper_housenumber}")
+                    response = 'y'
+                    time.sleep(1)
                 if response == '' or response.lower() == 'y' or response.lower() == u'з':
                     entity['tag']['addr:housenumber'] = proper_housenumber
             if entity_type == 'node':
