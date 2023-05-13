8ed31fec08382f51577ade441abe6322253dcc67
Branko Kokanovic <branko@kokanovic.org>
2023-05-13 20:46:21 +0200

    Smanjivanje frekvencije generisanja real-time izvestaja, unistava mi net kuci

--- src/upload_rt_report.sh
+++ src/upload_rt_report.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 current_date=`date +%Y%m%d%H%M`
 echo $current_date
 tar -czf data/rt-report$current_date.tar.gz -C data/report/rt/ .
-scp data/rt-report$current_date.tar.gz kokanovic:/home/branko/ar/rt-report$current_date.tar.gz
+scp -l 5000 data/rt-report$current_date.tar.gz kokanovic:/home/branko/ar/rt-report$current_date.tar.gz
 ssh kokanovic "tar -xzf /home/branko/ar/rt-report$current_date.tar.gz -C /var/www/sites/openstreetmap.rs/download/ar/rt/"
 ssh kokanovic "rm -f /home/branko/ar/rt-report$current_date.tar.gz"
 
