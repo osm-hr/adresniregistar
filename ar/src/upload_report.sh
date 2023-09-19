83dffcbedcef376c427898926ec6c78d89522f7f
Branko Kokanovic <branko@kokanovic.org>
2023-09-19 22:38:42 +0200

    Izbacivanje upload-a u stari dir

--- ar/src/upload_report.sh
+++ ar/src/upload_report.sh
@@ -6,7 +6,6 @@ current_date=`date +%Y%m%d`
 echo $current_date
 tar -czf data/temp-report$current_date.tar.gz -C data/report/ .
 scp data/temp-report$current_date.tar.gz kokanovic:/home/branko/ar/report$current_date.tar.gz
-ssh kokanovic "tar -xzf /home/branko/ar/report$current_date.tar.gz -C /var/www/sites/openstreetmap.rs/download/ar/"
 ssh kokanovic "tar -xzf /home/branko/ar/report$current_date.tar.gz -C /var/www/sites/dina.openstreetmap.rs/ar/"
 ssh kokanovic "rm -f /home/branko/ar/report$current_date.tar.gz"
 
--- ar/src/upload_rt_report.sh
+++ ar/src/upload_rt_report.sh
@@ -6,7 +6,6 @@ current_date=`date +%Y%m%d%H%M`
 echo $current_date
 tar -czf data/rt-report$current_date.tar.gz -C data/report/rt/ .
 scp -l 5000 data/rt-report$current_date.tar.gz kokanovic:/home/branko/ar/rt-report$current_date.tar.gz
-ssh kokanovic "tar -xzf /home/branko/ar/rt-report$current_date.tar.gz -C /var/www/sites/openstreetmap.rs/download/ar/rt/"
 ssh kokanovic "tar -xzf /home/branko/ar/rt-report$current_date.tar.gz -C /var/www/sites/dina.openstreetmap.rs/ar/rt/"
 ssh kokanovic "rm -f /home/branko/ar/rt-report$current_date.tar.gz"
 
