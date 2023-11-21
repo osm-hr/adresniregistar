4d41e9d3785237862a1b4ec12422ca4dd9234cf4
Branko Kokanovic <branko@kokanovic.org>
2023-11-21 22:24:26 +0100

    promena imena fajla za ST modul

--- st/src/upload_st_report.sh
+++ st/src/upload_st_report.sh
@@ -9,6 +9,6 @@ scp data/temp-report$current_date.tar.gz kokanovic:/home/branko/st/report$curren
 ssh kokanovic "tar -xzf /home/branko/st/report$current_date.tar.gz -C /var/www/sites/dina.openstreetmap.rs/st/"
 ssh kokanovic "rm -f /home/branko/st/report$current_date.tar.gz"
 
-mv data/temp-report$current_date.tar.gz data/report$current_date.tar.gz
+mv data/temp-report$current_date.tar.gz data/st-report$current_date.tar.gz
 
 rm -f data/running
