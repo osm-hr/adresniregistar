972769438214d67f305cf5d01b5055edcad5ecb6
Branko Kokanovic <branko@kokanovic.org>
2023-11-21 22:26:02 +0100

    helper skripta da restartujem overpass lakse

---
+++ ar/src/restart_overpass_rt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+test -f serbia.osm.pbf || wget http://download.geofabrik.de/europe/serbia-latest.osm.pbf -O serbia.osm.pbf -q --show-progress
+
+docker stop overpass_serbia_rt
+docker rm overpass_serbia_rt
+rm -rf /mnt/ubicassd/overpass_db/
+
+mkdir -p /mnt/ubicassd/overpass_db/
+
+docker run \
+  -e OVERPASS_META=yes \
+  -e OVERPASS_MODE=init \
+  -e OVERPASS_PLANET_URL=http://download.geofabrik.de/europe/serbia-latest.osm.pbf \
+  -e OVERPASS_DIFF_URL=https://planet.openstreetmap.org/replication/minute/ \
+  -e OVERPASS_RULES_LOAD=10 \
+  -e OVERPASS_PLANET_PREPROCESS='mv /db/planet.osm.bz2 /db/planet.osm.pbf && osmium cat -o /db/planet.osm.bz2 /db/planet.osm.pbf && rm /db/planet.osm.pbf' \
+  -v /mnt/ubicassd/overpass_db/:/db \
+  -p 12346:80 \
+  -i \
+  --name overpass_serbia_rt wiktorn/overpass-api
+
+docker start overpass_serbia_rt
+rm serbia.osm.pbf
