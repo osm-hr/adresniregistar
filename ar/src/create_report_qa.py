4bcad15de6f2c9846a90b79cd372a065cc34371d
Branko Kokanovic <branko@kokanovic.org>
2024-06-03 20:29:30 +0200

    ispravka pogresno generisanih .osm fajlova za prebacivanje adresa na zgradu

--- ar/src/create_report_qa.py
+++ ar/src/create_report_qa.py
@@ -91,7 +91,7 @@ def generate_osm_files_move_address_to_building(context, report_qa_address_path,
             'id': way_id,
             'tags': {k: xml_escape(v) for k, v in new_tags.items()},
             'nodes': entity['nodes'],
-            'version': entity['version'] + 1
+            'version': entity['version']
         })
         for node in entity['nodes']:
             node_entity = osm_entities_cache.nodes_cache[node]
