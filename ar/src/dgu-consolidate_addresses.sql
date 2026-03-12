cb9abfbfb2238454c360feb5c36666d39452e6cf
Janko Mihelić <janjko@gmail.com>
2026-03-12 02:06:01 +0100

    Dodane sve kolone na naselja, opcine i adrese.csv

--- ar/src/dgu-consolidate_addresses.sql
+++ ar/src/dgu-consolidate_addresses.sql
@@ -1,10 +1,10 @@
 SELECT
   regexp_replace(a.inspire_id, '^KB\.0*', '') AS rgz_kucni_broj_id,
+  o.opstina_maticni_broj AS rgz_opstina_mb,
+  o.opstina_imel AS rgz_opstina,
+  n.naselje_maticni_broj AS rgz_naselje_mb,
   ad.name AS rgz_naselje,
   ad.inspire_id AS rgz_naselje_inspire_id,
-  n.naselje_maticni_broj AS rgz_naselje_mb,
-  o.opstina_maticni_broj AS rgz_opstina_mb,
-  o.opstina_ime AS rgz_opstina,
   t.inspire_id AS rgz_ulica_mb,
   t.name AS rgz_ulica,
   a.designators AS rgz_kucni_broj,
@@ -14,5 +14,5 @@ FROM 'data/rgz/unzip/Address.csv' AS a
 JOIN 'data/rgz/unzip/ThoroughfareName.csv' AS t ON t.gml_id = a.thoroughfare_name_id
 JOIN 'data/rgz/unzip/PostalDescriptor.csv' AS p ON p.gml_id = a.postal_descriptor_id
 JOIN 'data/rgz/unzip/AdminUnitName.csv' AS ad ON ad.gml_id = a.admin_unit_1
-JOIN 'data/rgz/naselje.csv' AS n ON n.inspire_id = ad.inspire_id
-JOIN 'data/rgz/opstina.csv' AS o ON o.inspire_id = n.parent_id
+JOIN 'data/rgz/naselja.csv' AS n ON n.inspire_id = ad.inspire_id
+JOIN 'data/rgz/opstina.csv' AS o ON o.inspire_id = n.opstina_inspire_id
